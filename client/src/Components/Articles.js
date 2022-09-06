e2233e69068ab0863b0e1a8ce4c725f19bc3b39e
xsma-L <ismael.traore@epitech.eu>
2022-09-06 04:34:06 +0200

    step 1(url-parameter)

--- client/src/Components/Articles.js
+++ client/src/Components/Articles.js
@@ -6,15 +6,15 @@ import axios from 'axios';
 function Articles() {
     // Recuperation du parametre dans l'URL
     const [searchParams] = useSearchParams();
-    const params = searchParams.get("page");
+    const page = searchParams.get("page");
 
     let skip = null;
 
     // Calcul de la valeur de skip
-    if (params == 1) {
+    if (page == 1) {
         skip = 0;
     } else {
-        skip = `${params}0` - 10;
+        skip = `${page}0` - 10;
     }
     
     useEffect(() => {
